e6a3f1dfc646071428f097f514158a6d74cb7154
Amit Klein <AmitKlain1@gmail.com>
2021-04-03 09:18:06 +0300

    MONGODB Connected!

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 const express = require("express");
 const app = express();
 const morgan = require("morgan");
+const mongoose = require("mongoose");
+
+mongoose.connect(
+  `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@cluster0.xsgi4.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`,
+  { useNewUrlParser: true, useUnifiedTopology: true }
+);
+
+mongoose.connection.on("connected", () => {
+  console.log("MongoDB connected!");
+});
 
 const articlesRoutes = require("./api/routes/articles");
 const categoriesRoutes = require("./api/routes/catagories");
